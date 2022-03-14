69dd86bd7fd94ee63f7743182c74513fb9736e1b
Alan Morgan <alanmorgan@clearwateranalytics.com>
2022-03-13 22:18:16 -0600

    Drawing lines now.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
-//! This example showcases setting up a basic application and window delegate.
-//! Window Delegate's give you lifecycle methods that you can respond to.
-
 use core_graphics::geometry::{CGRect, CGPoint, CGSize};
+use core_graphics::context::{CGContext, CGContextRef};
+
 use cacao::layout::Layout;
 use cacao::macos::{App, AppDelegate};
 use cacao::macos::window::{Window, WindowStyle, WindowConfig, WindowDelegate};
 use cacao::color::{Color};
 use cacao::view::View;
 use cacao::objc::{msg_send, sel, sel_impl, class};
+use cacao::image::{ImageView, Image, DrawConfig};
+
 
 use std::os::raw::{c_char, c_int, c_uint, c_void};
 use cacao::objc::runtime::{Object, Class, objc_copyClassList};
 use cacao::foundation::{nil, YES, NO, NSString, NSInteger, NSUInteger};
 
-// Reference can be found here
-// https://github.com/phracker/MacOSX-SDKs/blob/041600eda65c6a668f66cb7d56b7d1da3e8bcc93/MacOSX10.10.sdk/System/Library/Frameworks/AppKit.framework/Versions/C/Headers/NSWindow.h
-// const NSWindowStyleMaskFullSizeContentView: i32 = 1 << 15;
 
 struct BasicApp {
     window: Window<MyWindow>
@@ -33,14 +31,56 @@ struct MyWindow {
     content: View,
 }
 
+fn draw_line(context: &CGContextRef, x1: f64, y1: f64, x2: f64, y2: f64) {
+    context.move_to_point(x1, y1);
+    context.add_line_to_point(x2, y2);
+}
+
 impl WindowDelegate for MyWindow {
     const NAME: &'static str = "MyWindow";
 
     fn did_load(&mut self, window: Window) {
-        self.content.set_background_color(Color::SystemBlue);
+        let image_view = ImageView::new();
+        let config = DrawConfig {
+            source: (12., 12.),
+            target: (100., 100.),
+            resize: cacao::image::ResizeBehavior::Stretch,
+        };
+
+        let image = Image::draw(config, |_cg_rect, context| {
+            let xmin = 3.;
+            let ymin = 3.;
+            let xmax = 10.;
+            let ymax = 10.;
+
+            context.move_to_point(xmin, ymin);
+            context.add_line_to_point(xmin,   ymax);
+            context.add_line_to_point(xmax, ymax);
+            context.add_line_to_point(xmax, ymin);
+            context.add_line_to_point(xmin,   ymin);
+            context.add_line_to_point(xmin,   ymax);
+
+            context.move_to_point(xmin, (ymax + ymin) / 2.);
+            context.add_line_to_point(xmax, (ymax + ymin) / 2.);
+
+            context.move_to_point((xmax + xmin) / 2., ymin);
+            context.add_line_to_point((xmax + xmin) / 2., ymax);
+
+            // draw_line(context, 0.,   0.,   0.,   ymax);
+            // draw_line(context, 0.,   ymax, xmax, ymax);
+            // draw_line(context, xmax, ymax, xmax, 0.);
+            // draw_line(context, xmax, 0.,   0.,   0.);
+
+            context.set_rgb_stroke_color(1., 0., 0., 1.);
+            context.stroke_path();
+            true
+        });
+
+        image_view.set_image(&image);
+        self.content.add_subview(&image_view);
         self.content.set_needs_display(true);
         self.content.set_translates_autoresizing_mask_into_constraints(true);
-        self.content.set_frame(CGRect::new(&CGPoint::new(-100., 100.), &CGSize::new(100., 100.)));
+        // self.content.set_frame(CGRect::new(&CGPoint::new(-100., 100.), &CGSize::new(100., 100.)));
         window.set_content_view(&self.content);
 
         // window.set_minimum_content_size(50,50);
@@ -49,14 +89,12 @@ impl WindowDelegate for MyWindow {
         // window.set_minimum_size(50,50);
 
         window.set_title_visibility(cacao::macos::window::TitleVisibility::Hidden);
-        window.set_background_color(Color::Clear);
+        // window.set_background_color(Color::Clear);
+        window.set_background_color(Color::SystemBlue);
         window.set_titlebar_appears_transparent(true);
         window.set_excluded_from_windows_menu(true);
         window.set_shows_toolbar_button(false);
         window.set_titlebar_appears_transparent(true);
-
-        // debug
-        window.set_movable_by_background(true);
 
         // Needed to move the window according to the screen.
         unsafe {
@@ -64,22 +102,9 @@ impl WindowDelegate for MyWindow {
             let _: () = msg_send![&*window.objc, setLevel:1 << 30];
             let _: () = msg_send![&*window.objc, setAnimationBehavior:nil];
         }
-    }
 
-    fn will_close(&self) {
-        println!("Closing now!");
-    }
-
-    fn will_enter_full_screen(&self) {
-        println!("FULL SCREEN now!");
-    }
-
-    fn will_move(&self) {
-        println!("Will move...");
-    }
-
-    fn did_move(&self) {
-        println!("Did move...");
+        // debug
+        // window.set_movable_by_background(true);
     }
 
     fn will_resize(&self, width: f64, height: f64) -> (f64, f64) {
@@ -90,11 +115,11 @@ impl WindowDelegate for MyWindow {
 
 fn main() {
     let mut config = WindowConfig::default();
-    config.set_initial_dimensions(100., 1100., 100., 100.);
+    config.set_initial_dimensions(0., 0., 100., 100.);
 
     config.set_styles(&[WindowStyle::Borderless]);
 
-    App::new("com.test.window-delegate", BasicApp {
+    App::new("xoc3.osxnav", BasicApp {
         window: Window::with(config, MyWindow::default())
     }).run();
 }
